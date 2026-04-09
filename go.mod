b96b8fab2f9cddaba783b8a15a071caa399a2871
Akhil Mohan <akhilerm@gmail.com>
2026-04-09 08:19:50 +0530

    feat: add IsEven function to check for even numbers
    This commit introduces the mathutils package and adds the IsEven
    function to determine if an integer is even.
    
    Signed-off-by: Akhil Mohan <akhilerm@gmail.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/akhilerm/sample-go-module
+
+go 1.25.7
---
+++ mathutils.go
@@ -0,0 +1,6 @@
+package mathutils
+
+// IsEven returns true if the given integer is even.
+func IsEven(n int) bool {
+	return n%2 == 0
+}
